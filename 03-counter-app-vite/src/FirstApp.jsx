5d879f5cefa3c3dc8ddd971e5be5ab2aefc45c1d
david sanchez <nagatiitoo@hotmail.com>
2023-04-11 14:16:04 -0500

    Counter app

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,4 +1,5 @@
-import {Fragment} from 'react'
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
 function getTitle() {
     return obj.title;
@@ -8,14 +9,26 @@ const obj = {
     text: "lorem ipsum dolor"
 }
 
-function FirstApp() {
+function FirstApp({ title, subtitle }) {
+
+
     return (
         <Fragment>
-            {/* <code>{JSON.stringify(obj)}</code> */}
+            {/* <code>{JSON.stringify(obj)}</code>
             <h1>{getTitle()}</h1>
-            <p>{obj.text}</p>
+            <p>{obj.text}</p> */}
+            <h1>{ title }</h1>
+            <p>{ subtitle }</p>
         </Fragment>
     )
 }
 
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.number
+}
+FirstApp.defaultProps = {
+    title: "Hola mundo",
+    subtitle: 1234
+}
 export default FirstApp
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-import App from './FirstApp'
+import App from './CounterApp'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <App value={12}/>
     </React.StrictMode>
 )
