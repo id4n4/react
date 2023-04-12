51c07055d89b42514bdfc668132983220981677d
david sanchez <nagatiitoo@hotmail.com>
2023-04-12 17:18:11 -0500

    update proyect 04-gif-expert-app

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,10 @@
 import PropTypes from 'prop-types'
 import { getGifs } from '../helpers/getGifs';
 import { useEffect } from 'react';
-import { useState } from 'react';
 
 
 
 function GifGrid({ category }) {
-    const [counter, setcounter] = useState(10)
     useEffect(() => {
         getGifs(category);
     }, [ ])
@@ -14,8 +12,6 @@ function GifGrid({ category }) {
         <>
             <h3>{category}</h3>
 
-            <h5>{counter}</h5>
-            <button onClick={() => setcounter(counter + 1)}>+1</button>
         </>
     )
 }
